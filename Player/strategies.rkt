604828d493a2b672c48a386ab5ae21e415a1b627
Matthias Felleisen <matthias@ccs.neu.edu>
2023-08-03 00:25:33 +0200

    clean up test inspection; abstract over search process

--- Player/strategies.rkt
+++ Player/strategies.rkt
@@ -7,11 +7,14 @@
  ;; if there are no tiles and the referee has enough for a 'swap', ask for a 'replacement
  ;; otherwise 'pass
  (contract-out
-  (d&g-strategy
+  [iterate-strategy
+   ;; iterate the strategy as far as possible to obtain a sqeuence of placements or PASS/REPALCEMENT
+   (-> (-> state? action?) state? action*?)]
+  (dag-strategy
    ;; strategy 1: dumb and greedy
    ;; choose "smallest tile" that has candidates; break tie among candidates via coodinate<
    (-> state? action?))
-  (nsd&g-strategy
+  (ldasg-strategy
    ;; strategy 2: a bit more sophisticate and still greedy
    ;; choose "smallest tile" that has candidates; pick most-constrained candidates;
    ;; break tie among candidates via coodinate<   
@@ -34,8 +37,8 @@
   (require rackunit))
 
 ;; ---------------------------------------------------------------------------------------------------
-#; Strategy 
-(define (d&g-strategy pk)
+#; {-> Strategy}
+(define ((make-strategy smallest) pk)
   (define gmap      (state-map pk))
   (define my-tiles  (sort (sop-tiles (first (state-players pk))) tile<))
   (define remaining (state-tiles pk))
@@ -45,10 +48,8 @@
     [(> (length my-tiles) remaining) PASS]
     [else REPLACEMENT]))
 
-#; {Map [Listof Tiles] -> Option<Placement>}
-(define (is-there-a-placement gmap mine)
-  (for*/first ([t (in-list mine)] [cs (in-value (find-candidates gmap t))] #:unless (set-empty? cs))
-    (list t cs)))
+;; ---------------------------------------------------------------------------------------------------
+;; dumb and greedy strategy: pick "smallest" coordinate of all feasible candidates for the first tile 
 
 #; {Tile [NonMTSet Candidate] -> Placement}
 (define (smallest t cs)
@@ -56,34 +57,21 @@
   (define sorted  (sort as-list top-down-left-to-right-order< #:key candidate-place))
   (placement (candidate-place (first sorted)) t))
 
+#; Strategy
+(define dag-strategy (make-strategy smallest))
+
 (module+ test
   (define the-special-place #s(placement #s(coordinate -5 1) #s(tile star green)))
   (define ref-place #s(placement #s(coordinate -1 0) #s(tile circle red)))
-  (check-equal? (d&g-strategy info-special-state) the-special-place)
-  (check-equal? (d&g-strategy info-+ref-starter-state) ref-place)
-  (check-equal? (d&g-strategy info-starter-state) REPLACEMENT)
-  (check-equal? (d&g-strategy info-bad-state) PASS))
+  (check-equal? (dag-strategy info-special-state) the-special-place)
+  (check-equal? (dag-strategy info-+ref-starter-state) ref-place)
+  (check-equal? (dag-strategy info-starter-state) REPLACEMENT)
+  (check-equal? (dag-strategy info-bad-state) PASS))
 
 ;; ---------------------------------------------------------------------------------------------------
-;; find candidates for tiles 
+;; less dumb & still greedy strategy: pick "most constrained" coordinate 
 
-#; Strategy 
-(define (nsd&g-strategy pk)
-  (define gmap      (state-map pk))
-  (define my-tiles  (sort (sop-tiles (first (state-players pk))) tile<))
-  (define remaining (state-tiles pk))
-  (define exists?   (are-there-candidates gmap my-tiles))
-  (cond
-    [exists? (apply most-constrained-placement exists?)]
-    [(> (length my-tiles) remaining) PASS]
-    [else REPLACEMENT]))
-
-#; {Map [Listof Tiles] -> [List Tile [Set Candidate]]}
-(define (are-there-candidates gmap mine)
-   (for*/first ([t (in-list mine)] [cs (in-value (find-candidates gmap t))] #:unless (set-empty? cs))
-     (list t cs)))
-
-#;{Tile [Setof Candidate] -> Placement}
+#; {Tile [NonMTSet Candidate] -> Placement}
 (define (most-constrained-placement t cs)
   (define with#  (for/list ([c (in-set cs)]) (list (candidate-constraints# c) c)))
   (define large* (pick-largest with#))
@@ -96,27 +84,32 @@
   (define mx (first (argmax first with#)))
   (filter-map (λ (x) (if (= (first x) mx) (second x) #false)) with#))
 
+#; Strategy
+(define ldasg-strategy (make-strategy most-constrained-placement))
+
 (module+ test
   (define constrained-special #s(placement #s(coordinate -3 1) #s(tile star green)))
-  (check-equal? (nsd&g-strategy info-special-state) constrained-special)
-  (check-equal? (nsd&g-strategy info-+ref-starter-state) ref-place)
-  (check-equal? (nsd&g-strategy info-starter-state) REPLACEMENT)
-  (check-equal? (nsd&g-strategy info-bad-state) PASS))
+  (check-equal? (ldasg-strategy info-special-state) constrained-special)
+  (check-equal? (ldasg-strategy info-+ref-starter-state) ref-place)
+  (check-equal? (ldasg-strategy info-starter-state) REPLACEMENT)
+  (check-equal? (ldasg-strategy info-bad-state) PASS))
+
+;; ---------------------------------------------------------------------------------------------------
+#; {Map [Listof Tiles] -> Option<Placement>}
+(define (is-there-a-placement gmap mine)
+  (for*/first ([t (in-list mine)] [cs (in-value (find-candidates gmap t))] #:unless (set-empty? cs))
+    (list t cs)))
 
 ;; ---------------------------------------------------------------------------------------------------
 
 #; {Strategy PubKnowledge -> (U PASS REPLACE [Listof Placement])}
 (define (iterate-strategy s pk0)
   (let until ([so-far '()] [pk pk0])
-    (set! pk* (cons pk pk*))
     (define action (s pk))
     (cond
       [(or (equal? PASS action) (equal? REPLACEMENT action))
        (if (empty? so-far) action (reverse so-far))]
       [else (until (cons action so-far) (apply-action pk action))])))
-
-(define pk* '())
-(define !!! (λ () (set! pk* '())))
 
 (module+ test
   (define info-special-place*
@@ -127,10 +120,10 @@
       #s(placement #s(coordinate -1 1) #s(tile diamond green))))
 
   ;; these have tiles that don't fit 
-  (check-equal? (iterate-strategy nsd&g-strategy info-bad-state) PASS)
-  (check-equal? (iterate-strategy d&g-strategy info-starter-state) REPLACEMENT)
+  (check-equal? (iterate-strategy ldasg-strategy info-bad-state) PASS)
+  (check-equal? (iterate-strategy dag-strategy info-starter-state) REPLACEMENT)
 
   ;; the following two run out of tiles
-  (check-equal? (iterate-strategy nsd&g-strategy info-special-state) info-special-places*dug)
-  (check-equal? (iterate-strategy d&g-strategy info-special-state) info-special-place*)
-  (check-equal? (iterate-strategy d&g-strategy info-+ref-starter-state) (list ref-place)))
+  (check-equal? (iterate-strategy ldasg-strategy info-special-state) info-special-places*dug)
+  (check-equal? (iterate-strategy dag-strategy info-special-state) info-special-place*)
+  (check-equal? (iterate-strategy dag-strategy info-+ref-starter-state) (list ref-place)))
