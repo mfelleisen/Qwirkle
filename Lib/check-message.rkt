23b9e1ffb08f8ce22271662605d94366d841247f
Matthias Felleisen <matthias@ccs.neu.edu>
2023-08-16 12:23:18 -0400

    improve error messaging

--- Lib/check-message.rkt
+++ Lib/check-message.rkt
@@ -12,16 +12,26 @@
 
 ;; -----------------------------------------------------------------------------
 (define-syntax-rule (check-message port rgxp body ...)
-  (let ([os (open-output-string)]
-        [mg (~a "looking for " rgxp)])
+  (let ([os  (open-output-string)]
+        [err #false]
+        [mg  (~a "looking for " rgxp)])
     (begin0
       (parameterize ([port os])
-        body ...)
+        (with-handlers ([exn:fail? (λ (xn) (set! err xn))])
+          body ...))
       (close-output-port os)
+      (when err
+        (eprintf "check-message: exn intercepted:\n")
+        (fprintf (current-error-port) (exn-message err))
+        (eprintf "\n------------------\n")
+        (raise err))
       (let* ([out (get-output-string os)]
              [ok? (cons? (regexp-match rgxp out))])
         (check-true ok? mg)
-        (unless ok? (eprintf "output string is ~s\n" out))))))
+        (unless ok?
+          (eprintf "output string is: \n")
+          (fprintf (current-error-port) out)
+          (eprintf "\n------------------\n"))))))
 
 (module+ test
   (check-message current-output-port #px"hello" (printf "hello")))
