5eb689563872145c079eb42e66f50a0f2f7582ca
Matthias Felleisen <matthias@ccs.neu.edu>
2023-12-02 16:54:24 -0500

    better name for remote proxy context

--- Remote/define-dispatcher.rkt
+++ Remote/define-dispatcher.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;; create a remote manager from types of the player's methods 
+;; create a remote proxy-context from types of the player's methods 
 
 ;                                                          
 ;                                                          
@@ -21,24 +21,24 @@
 ;                                                          
 
 (provide
- #; [define-remote-manager f c:dispatcher-clause ... c:dispatcher-clause]
+ #; [define-remote-proxy-context f c:dispatcher-clause ... c:dispatcher-clause]
  ;; where a dispatcher-clause has the shape 
  #; [(method:id arg:id ...) return:id]
- ;; introduces a remote manager named `f`
- ;; when the last clause matches, the manager shuts down
+ ;; introduces a remote proxy-context named `f`
+ ;; when the last clause matches, the proxy-context shuts down
 
  ;; The syntax assumes that functions called `jsexpr->arg` and `arg?` are defined in the context. 
  ;; These functions translate JSexprs to Racket values for the method.
  ;; Converserly, it also assumes that a function called `return->jsexpr` exists in the context.
  ;; These functions translate Racket values to JSexpres, which the receiver can send back to a server.
 
- define-remote-manager)
+ define-remote-proxy-context)
 
 #; (define create-reply/c   (-> (or/c eof-object? jsexpr?) jsexpr?))
 #; (define receiver/c       (-> create-reply/c any))
-#; (define remote-manager/c (-> receiver/c custodian? (-> manager-player/c any)))
+#; (define remote-proxy-context/c (-> receiver/c custodian? (-> proxy-context-player/c any)))
 
-;; the `remote-manager` for a player is a function that
+;; the `remote-proxy-context` for a player is a function that
 ;; -- repeatedly receives JSexpr and turns them into arguments so that it can 
 ;; -- call the appropriate method in the given player and then
 ;; -- turn the result into a JSexpr that can be sent back 
@@ -90,14 +90,14 @@
 ;                                                  
 ;                                                  
 
-(define (([make-remote-manager make-dispatcher] receiver custodian) player)
+(define (([make-remote-proxy-context make-dispatcher] receiver custodian) player)
   (define done? (box (gensym)))
   (define dispatcher (make-dispatcher player done?))
   (parameterize ([io-time-out 1000]
                  [error-print-width 1000])
     (let loop ()
       (with-handlers ([void (λ (xn)
-                                (eprintf "remote manager caught exn:\n ~a\n" (exn-message xn))
+                                (eprintf "remote proxy-context caught exn:\n ~a\n" (exn-message xn))
                                 (custodian-shutdown-all custodian))])
         (receiver dispatcher)
         (unless (boolean? (unbox done?)) (loop)))))
@@ -125,7 +125,7 @@
   (define-syntax-class dispatcher-clause
     (pattern [(method:id arg:id ...) return:id])))
 
-(define-syntax (define-remote-manager stx)
+(define-syntax (define-remote-proxy-context stx)
   (syntax-parse stx
     [[_ f c:dispatcher-clause ...]
      #:with ((pattern right-hand-side) ...) (map (match-clause #'player) (syntax->list #'[ c ...]))
@@ -143,7 +143,7 @@
                ...
                [patN (set-box! done? #true) rhsN]
                [ill (error 'dispatcher "server sent inappropriate message: ~e" ill)]))
-           (make-remote-manager make-dispatcher)))]))
+           (make-remote-proxy-context make-dispatcher)))]))
 
 (define-for-syntax ((match-clause player) stx)
   (syntax-parse stx
